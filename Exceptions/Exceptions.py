3b89e8675154a22bb3c328377610a3d08cc098d8
makecode24 <158132601+makecode24@users.noreply.github.com>
2024-01-30 13:44:49 -0500

    Add files via upload

---
+++ Exceptions/Exceptions.py
@@ -0,0 +1,12 @@
+class HttpConnectionException(Exception):
+    """
+    Throw this exception if there is error is quering delta table
+    """
+    pass
+
+class CustomSchemaException(Exception):
+    """
+    Throw this exception if there is error is schema delta table
+    """
+    pass
+
